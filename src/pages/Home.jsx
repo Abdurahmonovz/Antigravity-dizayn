82225a46ced93ea6797447a0d54386e52c7b78c0
maklersizkvartira <maklersizkvartira@gmail.com>
2026-08-27 11:51:47 +0500

    Update button link to bot with auto start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <header className="gallery-header">
         <h1>Dizayn Uslublari Galereyasi</h1>
         <p>Antigravityda o'z saytingizni dizaynini mana shu bo'limlardagi nomlar orqali o'z saytingizni jonlantiring!</p>
-        <a href="https://t.me/+hZr4ok72r4FmNmFi" target="_blank" rel="noopener noreferrer" className="telegram-btn">
+        <a href="https://t.me/Antigravity_tekshir_bot?start=1" target="_blank" rel="noopener noreferrer" className="telegram-btn">
           Bundanda qiziq ma'lumotlarni olish uchun tugmani bosing
         </a>
       </header>
